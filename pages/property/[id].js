e770e36cb74bf440e46dce35a5d362493ea87a06
LeynardChequito <infinitechojt2025@gmail.com>
2025-04-10 16:51:50 +0800

    Update [id].js

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -65,7 +65,6 @@ export default function PropertyPage({ recommended }) {
   };
 
   if (error) return <p className="text-center text-red-600 mt-10">{error}</p>;
-  if (!property) return <p className="text-center mt-10">Loading...</p>;
 
   return (
     <div className="dark:bg-gray-900">
